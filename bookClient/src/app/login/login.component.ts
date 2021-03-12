e53f70813d2ede2bb58b18078084c0b69edbdc5d
Nishanth <nishanthj@gsstvl.com>
2021-03-12 15:58:38 +0530

    Login Component Changes on 03/12/2021

--- bookClient/src/app/login/login.component.ts
+++ bookClient/src/app/login/login.component.ts
@@ -11,11 +11,16 @@ export class LoginComponent implements OnInit {
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.createLoginForm();
   }
   createLoginForm() {
     this.loginForm = this.fb.group({
-      UserName: ['', Validators.required],
-      password: ['', Validators.required]
+      EmailID: ['', [Validators.required, Validators.pattern(/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,10})$/)]],
+      Password: ['', Validators.required]
     });
+  }
+
+  get loginFormControls() {
+    return this.loginForm.controls;
   }
 }
